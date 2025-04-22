6b1ed423838eab9893bac5eebf49b24230c1b23f
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-22 14:14:41 +0200

    Added default package util, with basic Debug tool

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Executing program...");
-
+        System.out.println("Hello world!");
     }
 }
---
+++ src/util/DebugManager.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DebugManager {
+    public static boolean DEBUG = false;
+
+    public static void setDebug(boolean debug) {
+        DEBUG = debug;
+    }
+
+    public static boolean isDebug() {
+        return DEBUG;
+    }
+
+    public static void print(String message) {
+        if (DEBUG) {
+            System.out.println(getFormattedTime() + message + ConsoleColor.RESET);
+        }
+    }
+
+    private static String getFormattedTime() {
+        return "[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "] ";
+    }
+
+    public static void printError(String message) {
+        System.out.println(ConsoleColor.RED + getFormattedTime() + message + ConsoleColor.RESET);
+    }
+}
